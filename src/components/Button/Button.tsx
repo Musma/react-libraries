5a03d12d056d78756827c1361d95a947db7ffab9
Jinsoo Park <jinchuu1391@gmail.com>
2022-06-30 18:19:43 +0900

    feat: Storybook에서 컴포넌트 테스트 할 수 있도록 수정

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,53 @@
+import classNames from 'classnames'
+import { useCallback } from 'react'
+
+import { Body3 } from 'src/components/Typography'
+
+type Size = 'L' | 'M' | 'S' | 'XS'
+type Variant = 'outlined' | 'contained'
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   label: string
+  size?: Size
+  buttonClassName?: string
+  contentClassName?: string
+  variant?: Variant
+  // showIcon?: boolean
 }
 
-export const Button = ({ label, ...rest }: ButtonProps) => {
+export const Button = ({
+  label,
+  size = 'L',
+  buttonClassName,
+  contentClassName,
+  variant = 'contained',
+  // showIcon,
+  ...rest
+}: ButtonProps) => {
+  const getButtonClassName = useCallback(({ variant, size }: { variant: Variant; size: Size }) => {
+    const sizeOption = {
+      L: 'w-[200px]',
+      M: 'w-[144px]',
+      S: 'w-[92px]',
+      XS: 'w-[52px]',
+    }
+    const variantOption = {
+      outlined: `bg-white border border-[#036DB7] active:bg-white hover:bg-[#F2F8FB]`,
+      contained: `bg-[#036DB7] active:bg-[#025A96] hover:bg-[#036DB7]/90`,
+    }
+    return `${sizeOption[size]} ${variantOption[variant]}`
+  }, [])
+  const getContentClassName = useCallback((variant: Variant) => {
+    return variant === 'contained' ? 'text-white' : 'text-[#036DB7]'
+  }, [])
   return (
-    <button className="h-full w-10 bg-black" {...rest}>
-      {label}
+    <button
+      className={classNames(
+        `h-[34px] rounded-md active:shadow-[inset_-1px_2px_2px_rgba(0,0,0,0.25)] ${buttonClassName}`,
+        getButtonClassName({ variant, size }),
+      )}
+      {...rest}
+    >
+      <Body3 className={getContentClassName(variant)}>{label}</Body3>
     </button>
   )
 }
--- src/stories/Tooltip.stories.tsx
+++ src/stories/Tooltip.stories.tsx
@@ -6,13 +6,17 @@ import { Tooltip as _Tooltip } from 'src/components/Tooltip'
 export default {
   title: 'Components/Tooltip',
   component: _Tooltip,
-  argTypes: {},
 } as ComponentMeta<typeof _Tooltip>
 
-export const Tooltip: ComponentStory<typeof _Tooltip> = () => (
-  <div className="ml-[10vw] mt-[20vh] flex gap-x-2">
-    <_Tooltip message="Tooltip" position="top">
-      Hover me
-    </_Tooltip>
+const Template: ComponentStory<typeof _Tooltip> = (args) => (
+  <div className="ml-[20vw] mt-[10vh]">
+    <_Tooltip {...args} />
   </div>
 )
+
+export const Tooltip = Template.bind({})
+Tooltip.args = {
+  children: 'Hover me',
+  message: 'Tooltip',
+  position: 'left',
+}
