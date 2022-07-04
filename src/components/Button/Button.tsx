2fe2906d2d816bf7fd66f0d5dd6df7d9caf3d440
Jinsoo Park <jinchuu1391@gmail.com>
2022-07-04 18:03:14 +0900

    feat: Button 컴포넌트 props에 따라 다르게 css 처리

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,56 +1,72 @@
 import classNames from 'classnames'
 import React from 'react'
 
-import { Body3 } from 'src/components/Typography'
+import { Body3 } from '../Typography'
 
 type Size = 'L' | 'M' | 'S' | 'XS'
-type Variant = 'outlined' | 'contained'
+type Variant = 'outlined' | 'contained' | 'danger'
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   label: string
   size?: Size
   buttonClassName?: string
   contentClassName?: string
   variant?: Variant
+  disabled?: boolean
   // showIcon?: boolean
 }
 
 export const Button = ({
   label,
   size = 'L',
-  buttonClassName,
-  contentClassName,
+  buttonClassName = '',
+  contentClassName = '',
   variant = 'contained',
+  disabled = false,
   // showIcon,
   ...rest
 }: ButtonProps) => {
-  const getButtonClassName = React.useCallback(
-    ({ variant, size }: { variant: Variant; size: Size }) => {
-      const sizeOption = {
-        L: 'w-[200px]',
-        M: 'w-[144px]',
-        S: 'w-[92px]',
-        XS: 'w-[52px]',
-      }
-      const variantOption = {
-        outlined: `bg-white border border-[#036DB7] active:bg-white hover:bg-[#F2F8FB]`,
-        contained: `bg-[#036DB7] active:bg-[#025A96] hover:bg-[#036DB7]/90`,
-      }
-      return `${sizeOption[size]} ${variantOption[variant]}`
-    },
-    [],
-  )
-  const getContentClassName = React.useCallback((variant: Variant) => {
-    return variant === 'contained' ? 'text-white' : 'text-[#036DB7]'
+  const getSize = React.useCallback(() => {
+    const sizeOption = {
+      L: 'w-[200px]',
+      M: 'w-[144px]',
+      S: 'w-[92px]',
+      XS: 'w-[52px]',
+    }
+    return sizeOption[size]
+  }, [size])
+
+  const getVariantStyle = React.useCallback(() => {
+    const styles = {
+      outlined: 'bg-white border border-[#036DB7] active:bg-white hover:bg-[#F2F8FB]',
+      contained: 'bg-[#036DB7] active:bg-[#025A96] hover:bg-[#036DB7]/90',
+      danger: 'bg-[#CA3C3D] active:bg-[#A63033] hover:bg-[#CA3C3D]/90',
+    }
+    return `active:shadow-[inset_-1px_2px_2px_rgba(0,0,0,0.25)] ${styles[variant]}`
+  }, [variant])
+
+  const getDisabledStyle = React.useCallback(() => {
+    return 'bg-[#F9FAFB] cursor-not-allowed'
   }, [])
+
+  const getContentStyle = React.useCallback(() => {
+    if (disabled) return 'text-[#D0D5DD]'
+    if (variant === 'outlined') return 'text-[#036DB7]'
+    return 'text-white'
+  }, [variant, disabled])
+
   return (
     <button
       className={classNames(
-        `h-[34px] rounded-md active:shadow-[inset_-1px_2px_2px_rgba(0,0,0,0.25)] ${buttonClassName}`,
-        getButtonClassName({ variant, size }),
+        'h-[34px] rounded-md',
+        getSize(),
+        { [getVariantStyle()]: !disabled },
+        { [getDisabledStyle()]: disabled },
+        buttonClassName,
       )}
       {...rest}
+      disabled={disabled}
     >
-      <Body3 className={getContentClassName(variant)}>{label}</Body3>
+      <Body3 className={classNames(getContentStyle(), contentClassName)}>{label}</Body3>
     </button>
   )
 }
