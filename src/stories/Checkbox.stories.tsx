cc668e8cfcffc2c1d8234eeb7d069a428bc3c76b
Jinsoo Park <jinchuu1391@gmail.com>
2022-07-05 16:27:01 +0900

    feat: checkbox 추가

---
+++ src/stories/Checkbox.stories.tsx
@@ -0,0 +1,16 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { useState } from 'react'
+
+import { Checkbox as _Checkbox } from '../components/Checkbox'
+
+export default {
+  title: 'Components/Checkbox',
+  component: _Checkbox,
+} as ComponentMeta<typeof _Checkbox>
+
+const Template: ComponentStory<typeof _Checkbox> = (args) => {
+  const [checked, setChecked] = useState(false)
+  return <_Checkbox checked={checked} onChange={() => setChecked(!checked)} {...args} />
+}
+
+export const Checkbox = Template.bind({})
