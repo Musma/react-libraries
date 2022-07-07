62512677c8f7e0b81843de32e9bf67bdd05fc4f1
jklasd2001 <jklasd2001@naver.com>
2022-07-07 10:22:04 +0900

    feat: 불필요 의존성 제거 및 .storybook 하위 파일을 eslint 되도록 수정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,40 +1,37 @@
 // Imports the Storybook's configuration API
+import { resolve } from 'path'
+
 import type { StorybookViteConfig } from '@storybook/builder-vite'
-import { resolve } from 'path'  
-import svgrPlugin from 'vite-plugin-svgr'
+import svgr from 'vite-plugin-svgr'
 
 const config: StorybookViteConfig = {
   async viteFinal(config, { configType }) {
     // svgr 적용
-    config.plugins = [
-      ...config.plugins ?? [],
-      svgrPlugin(
-      )
-    ]
+    config.plugins = [...(config.plugins ?? []), svgr()]
     // 절대 경로
     config.resolve = {
       ...config.resolve,
       alias: {
         ...config.resolve?.alias,
-        src: resolve(__dirname, '../src/')
-      }
+        src: resolve(__dirname, '../src/'),
+      },
     }
     // return the customized config
-    return config;
+    return config
   },
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
-  framework: "@storybook/react",
+  framework: '@storybook/react',
   core: {
-    builder: "@storybook/builder-vite",
+    builder: '@storybook/builder-vite',
   },
   features: {
     storyStoreV7: true,
-  }
+  },
 }
 
 module.exports = config
