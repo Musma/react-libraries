2b0ba28622721afd9715cc1f04832ba44931c985
Jinsoo Park <jinchuu1391@gmail.com>
2022-07-07 10:57:09 +0900

    feat: RadioButton 구조변경

--- src/components/RadioButton/RadioButton.tsx
+++ src/components/RadioButton/RadioButton.tsx
@@ -27,55 +27,58 @@ export const RadioButton = ({
     return options[size]
   }, [size])
 
-  const width = useMemo(() => {
-    const options = {
+  const outerCircle = useMemo(() => {
+    const width = {
       lg: 'w-5',
       md: 'w-[13px]',
       sm: 'w-[10px]',
     }
-    return options[size]
-  }, [size])
+    const borderColor = {
+      checked: 'border-[#107C41]',
+      notChecked: 'border-[#BAC7D5]',
+      disabled: 'border-[#D0D5DD]',
+    }
+    if (disabled) return `${width[size]} ${borderColor.disabled}`
+    return `${width[size]} ${borderColor.notChecked} checked:${borderColor.checked}`
+  }, [size, disabled])
 
-  const textSize = useMemo(() => {
-    const options = {
+  const innerCircle = useMemo(() => {
+    const layout = {
+      lg: 'h-[11.25px] w-[11.25px] left-[4.375px] top-[4.375px]',
+      md: 'h-[7.31px] w-[7.31px] left-[2.845px] top-[2.845px]',
+      sm: 'h-[5.62px] w-[5.62px] left-[2.19px] top-[2.19px]',
+    }
+    const borderColor = {
+      disabled: 'border-[#D0D5DD] bg-[#D0D5DD]',
+      active: 'border-[#BAC7D5] peer-checked:border-[#107C41] peer-checked:bg-[#107C41]',
+    }
+    return `${layout[size]} ${disabled ? borderColor.disabled : borderColor.active}`
+  }, [size, disabled])
+
+  const labelFont = useMemo(() => {
+    const fontStyle = {
       lg: 'leading-5',
       md: 'text-[13px] leading-[13px]',
       sm: 'text-[10px] leading-[10px]',
     }
-    return options[size]
+    return fontStyle[size]
   }, [size])
 
-  const innerCircle = useMemo(() => {
-    const common = 'before:m-auto before:block before:rounded-full before:content-[""]'
-    const sizes = {
-      lg: 'before:h-[11.25px] before:w-[11.25px] before:m-[20%]',
-      md: 'before:h-[7.31px] before:w-[7.31px] before:my-[18%]',
-      sm: 'before:h-[5.62px] before:w-[5.62px] before:my-[15%]',
-    }
-    const state = {
-      disabled: 'border-[#D0D5DD] before:bg-[#D0D5DD]',
-      active: 'border-[#BAC7D5] checked:border-[#107C41] checked:before:bg-[#107C41]',
-    }
-    return `${common} ${sizes[size]} ${disabled ? state.disabled : state.active}`
-  }, [size, disabled])
-
   return (
-    <div className="flex items-center">
+    <label htmlFor={id} className="relative flex items-center">
       <input
         id={id}
         type="radio"
         className={classNames(
-          'cursor-pointer appearance-none rounded-full border',
+          'peer cursor-pointer appearance-none rounded-full border',
           height,
-          width,
-          innerCircle,
+          outerCircle,
         )}
         disabled={disabled}
         {...rest}
       />
-      <label htmlFor={id} className={classNames('ml-1', height, textSize, labelClassName)}>
-        {label}
-      </label>
-    </div>
+      <div className={classNames('absolute rounded-full', innerCircle)} />
+      <span className={classNames('ml-1', height, labelFont, labelClassName)}>{label}</span>
+    </label>
   )
 }
