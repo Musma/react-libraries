0bcbcebfb28bd5f40126a2a13cd532f4e9c145e0
Jinsoo Park <jinchuu1391@gmail.com>
2022-07-08 15:15:32 +0900

    chore: 미사용 코드 제거

--- src/stories/RadioButton.stories.tsx
+++ src/stories/RadioButton.stories.tsx
@@ -1,17 +1,14 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 
-import { RadioButton as _RadioButton, Props } from '../components/RadioButton'
+import { RadioButton as _RadioButton } from '../components/RadioButton'
 
 export default {
   title: 'Components/RadioButton',
   component: _RadioButton,
 } as ComponentMeta<typeof _RadioButton>
 
-export const Example: ComponentStory<typeof _RadioButton> = ({
-  label = 'Jason',
-  disabled,
-  ...rest
-}: Props) => {
+export const Example: ComponentStory<typeof _RadioButton> = (args) => {
+  const { label = 'Jason', disabled, ...rest } = args
   return (
     <div className="grid gap-y-2">
       <_RadioButton name="developer" label={label} disabled={disabled} {...rest} />
