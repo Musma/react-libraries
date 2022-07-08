6534924896c364d1c3d5c4212e9dccc18cb0fd7b
jklasd2001 <jklasd2001@naver.com>
2022-07-08 10:47:36 +0900

    feat: Pretendard 폰트 적용
    #9

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        pretendard: ["Pretendard", "-apple-system", "BlinkMacSystemFont", "system-ui", "Roboto", "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "sans-serif"]
+      }
+    },
   },
   darkMode: 'class',
   plugins: [],
