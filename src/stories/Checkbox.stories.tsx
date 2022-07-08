62d0b4e9c7341806a162559fc8cd83d474b56e69
Jinsoo Park <jinchuu1391@gmail.com>
2022-07-08 15:19:40 +0900

    fix: story파일에서 hook(useState) 관련 에러로 인해 예제 컴포넌트 분리

--- src/stories/Checkbox.stories.tsx
+++ src/stories/Checkbox.stories.tsx
@@ -1,20 +1,10 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react'
-import { useState } from 'react'
 
-import { Checkbox as _Checkbox } from '../components/Checkbox'
+import { Checkbox as _Checkbox, CheckboxExample } from '../components/Checkbox'
 
 export default {
   title: 'Components/Checkbox',
   component: _Checkbox,
 } as ComponentMeta<typeof _Checkbox>
 
-const Template: ComponentStory<typeof _Checkbox> = (args) => {
-  const [checked, setChecked] = useState(false)
-  return <_Checkbox checked={checked} onChange={() => setChecked(!checked)} {...args} />
-}
-
-export const Checkbox = Template.bind({})
-
-Checkbox.args = {
-  disabled: false,
-}
+export const Checkbox: ComponentStory<typeof _Checkbox> = (args) => <CheckboxExample {...args} />
