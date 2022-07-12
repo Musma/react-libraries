0a95a741c2ae463ebd70afe39e419aeb1b6baf17
Jinsoo Park <jinchuu1391@gmail.com>
2022-07-12 17:30:48 +0900

    chore: Size 타입 선언

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames'
-import _ from 'lodash'
+import _ from 'lodash-es'
 import { Fragment, ReactNode, useCallback, useEffect, useMemo, useRef } from 'react'
 
 import { ReactComponent as CloseIcon } from './images/close.svg'
@@ -7,10 +7,12 @@ import ModalManager from './ModalManager'
 
 import { SubTitle, Button } from 'src/components'
 import { useKeyEsc } from 'src/hooks/useKeyEsc'
+import { Size } from 'src/types'
+
 export interface ModalProps {
   title: string
   isOpen: boolean
-  size?: 'md' | 'sm'
+  size?: Extract<Size, 'md' | 'sm'>
   children: ReactNode
   buttonOption: ButtonOption
   isNested?: boolean
@@ -25,6 +27,12 @@ interface ButtonOption {
   onSecondClick?: () => void
 }
 
+/**
+ * 여러개의 모달이 중첩된 경우에 모달 종료 순서를 보장하기 위해 ModalManager를 사용합니다
+ * Escape keydown 이벤트 발생시 모달을 종료하는 이벤트핸들러를 설치한 경우, 화면상에 보이는 모달(마지막에 켜진 모달)이 아니라 먼저 켜진 순서대로 모달이 종료되는 문제가 있습니다.
+ * ModalManager에서 모달들을 관리하며, isTopModal 메서드를 통해 마지막에 켜진 모달인지 확인 후 모달을 종료시킬 수 있도록 구현했습니다.
+ *
+ */
 const manager = new ModalManager()
 
 export const Modal = ({
