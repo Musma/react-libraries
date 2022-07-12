15735851102f5cca882ddf9c6c514a7d56e88dd5
Jinsoo Park <jinchuu1391@gmail.com>
2022-07-12 15:34:44 +0900

    feat: TextInput 컴포넌트 추가

---
+++ src/stories/TextInput.stories.tsx
@@ -0,0 +1,25 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import { TextInput as _TextInput } from 'src/components/TextInput'
+
+export default {
+  title: 'Components/TextInput',
+  component: _TextInput,
+} as ComponentMeta<typeof _TextInput>
+
+const Template: ComponentStory<typeof _TextInput> = (args) => <_TextInput {...args} />
+export const Normal = Template.bind({})
+Normal.args = {
+  label: 'TextInput',
+}
+export const Valid = Template.bind({})
+Valid.args = {
+  label: 'TextInput',
+  type: 'password',
+  helperText: { type: 'valid', message: 'Assistive Text' },
+}
+export const Invalid = Template.bind({})
+Invalid.args = {
+  label: 'TextInput',
+  helperText: { type: 'invalid', message: 'Assistive Text' },
+}
