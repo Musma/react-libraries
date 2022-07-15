a5658ec23b0cfefb39c1efbc1501cbc47fa2c37e
jklasd2001 <jklasd2001@naver.com>
2022-07-15 14:46:24 +0900

    feat: Storybook 제거

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   plugins: {
+    // Nesting CSS 사용하기 위함. 
+    // index.css
+    // @layer utilities 안의 .custom-scroll
+    // https://tailwindcss.com/docs/using-with-preprocessors#nesting
+    // 'tailwindcss/nesting': {}, 
     tailwindcss: {},
     autoprefixer: {},
   },
