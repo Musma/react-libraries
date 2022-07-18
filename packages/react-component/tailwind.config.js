19f6aadf3781d0e7a6497cf748eacef06142c69a
jklasd2001 <jklasd2001@naver.com>
2022-07-18 09:13:41 +0900

    feat: Pnpm workspace 구성

--- packages/react-component/tailwind.config.js
+++ packages/react-component/tailwind.config.js
@@ -5,18 +5,6 @@ module.exports = {
       fontFamily: {
         pretendard: ["Pretendard", "-apple-system", "BlinkMacSystemFont", "system-ui", "Roboto", "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "sans-serif"]
       },
-      colors: {
-        blue: {
-          main: '',
-          dark: '',
-          light: '',
-          lighter: '',
-        },
-        black: {
-          main: '',
-          light: '',
-        }
-      }
     },
   },
   darkMode: 'class',
