5783c3d607b33d661a3a82ed74de83c425ec6f8e
jklasd2001 <jklasd2001@naver.com>
2022-07-27 16:57:11 +0900

    feat: Divider 컴포넌트 추가

---
+++ packages/react-component/src/components/Divider/Divider.tsx
@@ -0,0 +1,19 @@
+import classNames from 'classnames'
+import { HTMLAttributes } from 'react'
+
+interface DividerProps extends HTMLAttributes<HTMLHRElement> {
+  orientation?: 'horizontal' | 'vertical'
+}
+
+export const Divider = ({ orientation = 'horizontal', className, ...rest }: DividerProps) => {
+  return (
+    <hr
+      {...rest}
+      className={classNames(
+        `m-0 flex-shrink-0 border-[#BAC7D5]`,
+        orientation === 'vertical' && 'h-auto self-stretch border-r-[1px]',
+        className,
+      )}
+    />
+  )
+}
