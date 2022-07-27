18adcee8759b97d9f911f0ac2d20acfa81b93828
Jinsoo Park <jinchuu1391@gmail.com>
2022-07-27 16:46:16 +0900

    feat: Breadcrumb 컴포넌트 추가

--- packages/react-component/src/components/Breadcrumb/Breadcrumb.tsx
+++ packages/react-component/src/components/Breadcrumb/Breadcrumb.tsx
@@ -1,3 +1,46 @@
-export const Breadcrumb = () => {
-  return <div>asopdkaspodk</div>
+import { useCallback } from 'react'
+
+import { Paragraph } from '../Paragraph'
+import { ReactComponent as HomeIcon } from './images/home.svg'
+import { ReactComponent as SeperatorIcon } from './images/seperator.svg'
+
+interface BreadCrumbProps {
+  crumbs: string[]
+  onClick: (crumb: string) => void
+}
+
+export const Breadcrumb = ({ crumbs, onClick }: BreadCrumbProps) => {
+  const isLast = useCallback(
+    (index: number) => {
+      return index === crumbs.length - 1
+    },
+    [crumbs],
+  )
+
+  return (
+    <nav>
+      <ol className="flex items-center">
+        {crumbs.map((crumb, index) => {
+          if (index === 0) {
+            return (
+              <HomeIcon className="cursor-pointer" fill="#036DB7" onClick={() => onClick(crumb)} />
+            )
+          }
+          return (
+            <li key={index} className="flex">
+              <SeperatorIcon />
+              <button onClick={() => onClick(crumb)}>
+                <Paragraph
+                  element="p2"
+                  className={isLast(index) ? 'text-[#242E40]' : 'text-[#D0D5DD]'}
+                >
+                  {crumb}
+                </Paragraph>
+              </button>
+            </li>
+          )
+        })}
+      </ol>
+    </nav>
+  )
 }
