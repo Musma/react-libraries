7a67c7c59629bb2b624255c69139b12ac927dcbc
jklasd2001 <jklasd2001@naver.com>
2022-07-27 16:57:56 +0900

    feat: Toast 컴포넌트 추가(미완성)

---
+++ packages/react-component/src/components/Toast/Toast.tsx
@@ -0,0 +1,7 @@
+interface ToastProps {
+  variant?: 'info' | 'success' | 'error' | 'warning' | 'checked'
+}
+
+export const Toast = ({ variant }: ToastProps) => {
+  return <div>asp odkaspodksaop</div>
+}
