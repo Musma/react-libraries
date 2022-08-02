5ad33c730ddf4e409ceae4ed93b29b64be08fd1f
Jinsoo Park <jinchuu1391@gmail.com>
2022-08-02 14:35:45 +0900

    feat: Select 컴포넌트에 변경된 사이즈 타입 적용

--- packages/react-component/src/App.tsx
+++ packages/react-component/src/App.tsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 import { useModalManager } from 'src/components/Modal/useModalManager'
 
-import { Button, Modal, Tag, useTags } from './components'
+import { Button, Modal, Select, Tag, useTags } from './components'
 
 const App = () => {
   const swTeam = useTags(['dyson', 'bob', 'kuhn'])
-  const [open, setOpen] = useState(false)
+  const [selected, setSelected] = useState('')
   const manager = useModalManager()
   return (
     <div className="h-full w-full">
@@ -15,16 +15,39 @@ const App = () => {
             header 입니다
           </div>
         </header>
-        <Button label="modal open" onClick={() => setOpen(true)} />
-        <Modal
-          buttonOption={{ label: 'label' }}
-          title={'hh'}
-          isOpen={open}
-          modalManager={manager}
-          onClose={() => setOpen(false)}
-        >
-          hh
-        </Modal>
+        selected: {selected}
+        <div>
+          <Select
+            size="lg"
+            label={'라벨'}
+            value={selected}
+            options={['jason', 'jelly', 'summer', 'justin', 'hi', 'hello', '안녕'].map((item) => ({
+              label: item,
+              value: item,
+            }))}
+            onChange={(value) => setSelected(value)}
+          />
+          <Select
+            size="md"
+            label={'라벨'}
+            value={selected}
+            options={['jason', 'jelly', 'summer', 'justin', 'hi', 'hello', '안녕'].map((item) => ({
+              label: item,
+              value: item,
+            }))}
+            onChange={(value) => setSelected(value)}
+          />
+          <Select
+            size="sm"
+            label={'라벨'}
+            value={selected}
+            options={['jason', 'jelly', 'summer', 'justin', 'hi', 'hello', '안녕'].map((item) => ({
+              label: item,
+              value: item,
+            }))}
+            onChange={(value) => setSelected(value)}
+          />
+        </div>
       </div>
     </div>
   )
--- packages/react-component/src/components/Select/Select.tsx
+++ packages/react-component/src/components/Select/Select.tsx
@@ -1,56 +1,252 @@
 import classNames from 'classnames'
-import { useMemo } from 'react'
+import { ChangeEvent, useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import { Sizes } from 'src/types'
 
-export interface SelectOption<T> {
+import { Typography } from '../Typography'
+import { ReactComponent as ArrowTopIcon } from './images/arrow_top.svg'
+
+interface SelectProps {
+  size: Sizes
   label: string
-  value: T
+  value: string
+  options: { label: string; value: string }[]
+  onChange: (value: string) => void
 }
 
-interface SelectProps<T> {
-  label?: string
-  value: T
-  options: SelectOption<T>[]
-  onChange: (value: T) => void
-  id?: string
-}
+export const Select = ({ size, label, value, options, onChange }: SelectProps) => {
+  const inputRef = useRef<HTMLInputElement>(null)
+  const divRef = useRef<HTMLDivElement>(null)
+  const getLabel = useCallback(
+    (value: string) => {
+      const selected = options.find((option) => option.value === value)
+      if (!selected) {
+        return ''
+      }
+      return selected.label
+    },
+    [options],
+  )
 
-export const Select = <T,>({ value, label, options, onChange }: SelectProps<T>) => {
-  const selectedItem = useMemo(() => {
-    if (value) {
-      const item = options.find((it) => it.value === value)
-      return item
+  const [text, setText] = useState('')
+  const [isOpen, setIsOpen] = useState(false)
+  // FIXME: 키보드로 옵션 선택할 수 있는 기능 구현중 선택된 항목을 따라 스크롤이 움직이는 부분에서 막혔음... 추후 구현
+  // const [pointer, setPointer] = useState(0)
+
+  const filteredOptions = useMemo(() => {
+    if (!text) return options
+    return options.filter((option) => option.label.includes(text))
+  }, [options, text])
+
+  const isValid = useCallback(
+    (text: string) => {
+      if (!text) return
+      return options.find((option) => option.label === text)
+    },
+    [options],
+  )
+
+  // FIXME: 키보드로 옵션 선택할 수 있는 기능 구현중 선택된 항목을 따라 스크롤이 움직이는 부분에서 막혔음... 추후 구현
+  // const upPointer = useCallback(() => {
+  //   if (pointer === 0) {
+  //     setPointer(filteredOptions.length - 1)
+  //     return
+  //   }
+  //   setPointer((prev) => prev - 1)
+  // }, [filteredOptions.length, pointer])
+
+  // const downPointer = useCallback(() => {
+  //   if (pointer === filteredOptions.length - 1) {
+  //     setPointer(0)
+  //     return
+  //   }
+  //   setPointer((prev) => prev + 1)
+  // }, [filteredOptions.length, pointer])
+
+  const handleTextChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value)
+  }, [])
+
+  const handleOptionSelect = useCallback(
+    (value: string) => {
+      onChange(value)
+      setIsOpen(false)
+    },
+    [onChange],
+  )
+  // FIXME: 키보드로 옵션 선택할 수 있는 기능 구현중 선택된 항목을 따라 스크롤이 움직이는 부분에서 막혔음... 추후 구현
+  // const handleKeyPress = useCallback(
+  //   (e: React.KeyboardEvent<HTMLInputElement>) => {
+  //     if (!isOpen && e.key === 'ArrowDown') {
+  //       setIsOpen(true)
+  //     }
+  //     if (e.key === 'Enter' && pointer >= 0) {
+  //       handleOptionSelect(filteredOptions[pointer].value)
+  //       return
+  //     }
+  //     if (e.key === 'Enter' && isValid(e.target.value)) {
+  //       handleOptionSelect(e.target.value)
+  //     }
+  //   },
+  //   [filteredOptions, handleOptionSelect, isOpen, isValid, pointer],
+  // )
+
+  const handleDropdownClick = useCallback(() => {
+    if (!inputRef.current) return
+    if (!isOpen) {
+      setIsOpen(true)
+      inputRef.current.focus()
+      return
     }
-  }, [value, options])
+    setIsOpen(false)
+  }, [isOpen])
 
-  console.log(selectedItem?.value)
+  const isSelected = useCallback(
+    (v: string) => {
+      return value === v
+    },
+    [value],
+  )
+
+  useEffect(() => {
+    const selectOption = (e: MouseEvent) => {
+      if (!divRef.current) return
+      if (divRef.current.contains(e.target as Node)) return
+      if (isValid(text)) {
+        handleOptionSelect(text)
+        return
+      }
+      setText('')
+      setIsOpen(false)
+    }
+    document.addEventListener('click', selectOption)
+    return function cleanup() {
+      document.removeEventListener('click', selectOption)
+    }
+  }, [getLabel, handleOptionSelect, isValid, onChange, text, value])
+
+  // FIXME: 키보드로 옵션 선택할 수 있는 기능 구현중 선택된 항목을 따라 스크롤이 움직이는 부분에서 막혔음... 추후 구현
+  // useEffect(() => {
+  //   if (!isOpen) return
+  //   const handleUpDown = (e: KeyboardEvent) => {
+  //     if (e.key === 'ArrowDown') {
+  //       downPointer()
+  //       return
+  //     }
+  //     if (e.key === 'ArrowUp') {
+  //       upPointer()
+  //       return
+  //     }
+  //   }
+  //   document.addEventListener('keydown', handleUpDown)
+  //   return function cleanup() {
+  //     document.removeEventListener('keydown', handleUpDown)
+  //   }
+  // }, [downPointer, filteredOptions, isOpen, options.length, pointer, upPointer])
+
+  const inputSize = useMemo(() => {
+    return {
+      lg: 'h-8 w-[200px] text-[14px]',
+      md: 'h-7 w-[180px] text-[12px]',
+      sm: 'h-6 w-[148px] text-[12px]',
+    }[size]
+  }, [size])
+
+  const arrowPosition = useMemo(() => {
+    return {
+      lg: 'top-1 right-2',
+      md: 'top-[2px] right-1',
+      sm: 'top-0 right-0',
+    }[size]
+  }, [size])
 
   return (
-    <div className="inline-flex flex-col">
-      {label && <label>{label}</label>}
-      <div className="group relative" tabIndex={-1}>
+    <div className="inline-flex flex-col items-start">
+      <label htmlFor="input">
+        <TitleFactory size={size} label={label} className="mb-[2px]" />
+      </label>
+      <div ref={divRef} className="relative" onClick={handleDropdownClick}>
         <input
-          className="rounded-[4px] border border-[#BAC7D5] outline-none focus-within:ring-1"
-          value={value as unknown as string}
-          readOnly={true}
+          id={'input'}
+          ref={inputRef}
+          value={text}
+          onChange={handleTextChange}
+          // onKeyDown={handleKeyPress}
+          className={classNames(
+            'cursor-pointer rounded border border-[#BAC7D5] pl-2 leading-5 outline-none focus:border-[#036DB7]',
+            inputSize,
+          )}
         />
-        <div className="bg-black custom-scrollbar absolute top-[calc(100%_+_8px)] hidden max-h-[158px] w-full flex-col overflow-y-auto rounded-[4px] border border-[#BAC7D5] p-1 outline-none group-focus-within:flex">
-          {options.map((option, index) => (
-            <div
-              key={index}
-              className={classNames(
-                selectedItem?.value === option.value ? 'text-[#036DB7]' : 'text-[#242E40]',
-                `mb-1 cursor-pointer text-[14px] last-of-type:mb-0 hover:text-[#036DB7]`,
-              )}
-              role="option"
-              onClick={() => {
-                onChange(option.value)
-              }}
-            >
-              {option.label}
-            </div>
-          ))}
-        </div>
+        <LabelFactory
+          size={size}
+          label={getLabel(value)}
+          className={classNames(
+            'absolute',
+            { 'left-[10px] top-[6px]': size !== 'sm' },
+            { 'left-[9px] top-[5px]': size === 'sm' },
+            { invisible: text },
+          )}
+        />
+        <ArrowTopIcon
+          className={classNames('absolute cursor-pointer', arrowPosition, {
+            'rotate-180': isOpen,
+          })}
+        />
       </div>
+
+      {isOpen && (
+        <ul className="mt-1 grid max-h-[158px] w-full grid-cols-1 gap-y-[2px] overflow-scroll rounded border border-[#BAC7D5] py-1">
+          {filteredOptions.map(({ label, value }, index) => (
+            <li
+              key={label}
+              onClick={() => handleOptionSelect(value)}
+              className={classNames(
+                'h-8 cursor-pointer py-1 pl-4',
+                { 'bg-[#036DB7] text-white': isSelected(value) },
+                { 'hover:bg-[#F2F8FB] hover:text-[#036DB7]': !isSelected(value) },
+                // FIXME: 키보드로 옵션 선택할 수 있는 기능 구현중 선택된 항목을 따라 스크롤이 움직이는 부분에서 막혔음... 추후 구현
+                // { 'active bg-[#F2F8FB] text-[#036DB7]': !isSelected(value) && index === pointer },
+              )}
+            >
+              <LabelFactory size={size} label={label} />
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
+  )
+}
+
+interface TypographyFactoryProps {
+  size: Sizes
+  label: string
+  className?: string
+}
+const LabelFactory = ({ size, label, className = '' }: TypographyFactoryProps) => {
+  if (size === 'lg') {
+    return (
+      <Typography type="body" variant="body3" className={className}>
+        {label}
+      </Typography>
+    )
+  }
+  return (
+    <Typography type="caption" className={className}>
+      {label}
+    </Typography>
+  )
+}
+
+const TitleFactory = ({ size, label, className }: TypographyFactoryProps) => {
+  if (size === 'lg') {
+    return (
+      <Typography type="subTitle" variant="subTitle2" className={className}>
+        {label}
+      </Typography>
+    )
+  }
+  return (
+    <Typography type="subTitle" variant="subTitle3" className={className}>
+      {label}
+    </Typography>
   )
 }
