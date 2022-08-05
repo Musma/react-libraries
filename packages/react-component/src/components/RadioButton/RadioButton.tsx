7916bdd3640322d42a7adf1a7ac363d279d79357
Jinsoo Park <jinchuu1391@gmail.com>
2022-08-05 15:39:12 +0900

    feat: RadioButton 수정

--- packages/react-component/src/components/RadioButton/RadioButton.tsx
+++ packages/react-component/src/components/RadioButton/RadioButton.tsx
@@ -1,86 +1,81 @@
-import classNames from 'classnames'
-import _ from 'lodash-es'
-import { useMemo } from 'react'
+import { useCallback } from 'react'
 import { Size } from 'src/types'
 
 import { Typography } from '../Typography'
+import {
+  ReactComponent as LgCheckedIcon,
+  ReactComponent as MdCheckedIcon,
+  ReactComponent as SmCheckedIcon,
+} from './images/checked_lg.svg'
+import { ReactComponent as LgDefaultIcon } from './images/default_lg.svg'
+import { ReactComponent as MdDefaultIcon } from './images/default_md.svg'
+import { ReactComponent as SmDefaultIcon } from './images/default_sm.svg'
+import { ReactComponent as LgDisabledIcon } from './images/disabled_lg.svg'
+import { ReactComponent as MdDisabledIcon } from './images/disabled_md.svg'
+import { ReactComponent as SmDisabledIcon } from './images/disabled_sm.svg'
 
-interface RadioButtonProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'size'> {
+interface RadioButtonProps {
   label?: string
   size?: Size
+  value: boolean
+  onChange: () => void
   labelClassName?: string
   disabled?: boolean
 }
 
 export const RadioButton = ({
-  id = _.uniqueId(),
   label,
+  value,
   size = 'lg',
+  onChange,
+  labelClassName,
   disabled = false,
-  ...rest
 }: RadioButtonProps) => {
-  const height = useMemo(() => {
-    const options = {
-      lg: 'h-[21px]',
-      md: 'h-[14px]',
-      sm: 'h-[12px]',
-    }
-    return options[size]
-  }, [size])
-
-  const outerCircle = useMemo(() => {
-    const width = {
-      lg: 'w-[21px]',
-      md: 'w-[14px]',
-      sm: 'w-[12px]',
-    }
-    const borderColor = {
-      checked: 'border-[#107C41]',
-      notChecked: 'border-[#BAC7D5]',
-      disabled: 'border-[#D0D5DD]',
-    }
-    if (disabled) return `${width[size]} ${borderColor.disabled}`
-    return `${width[size]} ${borderColor.notChecked} checked:${borderColor.checked}`
-  }, [size, disabled])
-
-  const innerCircle = useMemo(() => {
-    const layout = {
-      lg: 'h-[11.81px] w-[11.81px] left-[6.09px] top-[6.09px]',
-      md: 'h-[7.88px] w-[7.88px] left-[4.06px] top-[4.06px]',
-      sm: 'h-[6.75px] w-[6.75px] left-[3.62px] top-[3.62px]',
-    }
-    const borderColor = {
-      disabled: 'border-[#D0D5DD] bg-[#D0D5DD]',
-      active: 'border-[#BAC7D5] peer-checked:border-[#107C41] peer-checked:bg-[#107C41]',
-    }
-    return `${layout[size]} ${disabled ? borderColor.disabled : borderColor.active}`
-  }, [size, disabled])
-
-  const container = useMemo(() => {
-    return {
-      lg: 'w-6 h-6',
-      md: 'w-4 h-4',
-      sm: 'w-[14px] h-[14px]',
-    }[size]
-  }, [size])
-
+  const getCircle = useCallback(() => {
+    if (disabled) return <Disabled size={size} />
+    if (value) return <Checked size={size} />
+    return <Default size={size} />
+  }, [disabled, size, value])
   return (
-    <label htmlFor="id" className="flex items-center">
-      <div className={classNames('relative flex items-center justify-center', container)}>
-        <input
-          id={id}
-          type="radio"
-          className={classNames(
-            'peer cursor-pointer appearance-none rounded-full border',
-            height,
-            outerCircle,
-          )}
-          disabled={disabled}
-          {...rest}
-        />
-        <div className={classNames('absolute rounded-full', innerCircle)} />
-      </div>
-      <Typography>{label}</Typography>
-    </label>
+    <div onClick={onChange} className="flex items-center">
+      {getCircle()}
+      <Typography className={labelClassName}>{label}</Typography>
+    </div>
   )
+}
+
+interface CircleProps {
+  size: Size
+}
+const Checked = ({ size }: CircleProps) => {
+  switch (size) {
+    case 'lg':
+      return <LgCheckedIcon className="cursor-pointer" />
+    case 'md':
+      return <MdCheckedIcon className="cursor-pointer" />
+    case 'sm':
+      return <SmCheckedIcon className="cursor-pointer" />
+  }
+}
+
+const Default = ({ size }: CircleProps) => {
+  switch (size) {
+    case 'lg':
+      return <LgDefaultIcon className="cursor-pointer" />
+    case 'md':
+      return <MdDefaultIcon className="cursor-pointer" />
+    case 'sm':
+      return <SmDefaultIcon className="cursor-pointer" />
+  }
+}
+
+const Disabled = ({ size }: CircleProps) => {
+  switch (size) {
+    case 'lg':
+      return <LgDisabledIcon className="cursor-not-allowed" />
+    case 'md':
+      return <MdDisabledIcon className="cursor-not-allowed" />
+    case 'sm':
+      return <SmDisabledIcon className="cursor-not-allowed" />
+  }
 }
