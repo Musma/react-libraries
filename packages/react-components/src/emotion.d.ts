e06fd281a95b883e40b2c1b4d90787f3287025bf
Jinsoo Park <jinchuu1391@gmail.com>
2022-08-23 11:25:54 +0900

    chore: 테마 설정, 웹폰트 추가

---
+++ packages/react-components/src/emotion.d.ts
@@ -0,0 +1,57 @@
+import '@emotion/react'
+
+declare module '@emotion/react' {
+  export interface Theme {
+    color: {
+      blue: {
+        lighter: string
+        light: string
+        main: string
+        dark: string
+        darker: string
+      }
+      black: {
+        lighter: string
+        light: string
+        main: string
+        dark: string
+        darker: string
+      }
+      gray: {
+        lighter: string
+        light: string
+        main: string
+        dark: string
+        darker: string
+      }
+      white: {
+        lighter: string
+        light: string
+        main: string
+        dark: string
+        darker: string
+      }
+      red: {
+        lighter: string
+        light: string
+        main: string
+        dark: string
+        darker: string
+      }
+      green: {
+        lighter: string
+        light: string
+        main: string
+        dark: string
+        darker: string
+      }
+      orange: {
+        lighter: string
+        light: string
+        main: string
+        dark: string
+        darker: string
+      }
+    }
+  }
+}
--- packages/react-components/src/main.tsx
+++ packages/react-components/src/main.tsx
@@ -1,10 +1,39 @@
+import { css, Global, ThemeProvider } from '@emotion/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import font1 from './fonts/Pretendard-Regular.subset.woff2'
+import { theme } from './styles/theme'
+import normalize from 'normalize.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Global
+      styles={css`
+        ${normalize}
+        :root {
+          font-synthesis: none;
+          text-rendering: optimizeLegibility;
+          -webkit-font-smoothing: antialiased;
+          -moz-osx-font-smoothing: grayscale;
+          -webkit-text-size-adjust: 100%;
+        }
+        @font-face {
+          font-family: 'Pretendard';
+          font-style: normal;
+          font-weight: 400;
+          src: local('Pretendard'), url(${font1}) format('woff2');
+        }
+        body {
+          font-family: 'Pretendard';
+          margin: 0;
+          min-width: 320px;
+          min-height: 100vh;
+        }
+      `}
+    />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
