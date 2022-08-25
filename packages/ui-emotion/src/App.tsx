bf88cb5ad7e6b8fd0632d557b050c775797aae57
Jinsoo Park <jinchuu1391@gmail.com>
2022-08-25 17:03:52 +0900

    style: 메인 페이지 레이아웃 수정

--- packages/ui-emotion/src/App.tsx
+++ packages/ui-emotion/src/App.tsx
@@ -1,31 +1,78 @@
 import { css } from '@emotion/css'
+import { useTheme } from '@emotion/react'
+import { ReactNode } from 'react'
 import { Badge, BreadCrumb, Tooltip, Button, Typography } from './components'
+import { Tag } from './components/Tag'
 
 function App() {
   return (
-    <div className={css({ padding: 32 })}>
-      <Typography type="heading" variant="h3">
-        Typography
-      </Typography>
-      <Typography>body type typography</Typography>
-      <Typography type="heading" variant="h3">
-        Button
-      </Typography>
-      <Button className={css({ width: '100px' })} variant="danger" disabled={true} label="test" />
-      <Typography type="heading" variant="h3">
-        Badge
-      </Typography>
-      <Badge count={222}>badge</Badge>
-      <Typography type="heading" variant="h3">
-        BreadCrumb
-      </Typography>
-      <BreadCrumb crumbs={['a', 'b', 'c']} onClick={(c) => console.log(c)} />
-      <Typography type="heading" variant="h3">
-        Tooltip
-      </Typography>
-      <Tooltip message="world">hello</Tooltip>
+    <div
+      className={css({
+        padding: 32,
+        display: 'grid',
+        gridTemplateRows: 'auto',
+        gridTemplateColumns: 'repeat(4, 400px)',
+      })}
+    >
+      <Box title="typography">
+        <Typography>body type typography</Typography>
+      </Box>
+      <Box title="button">
+        <Button className={css({ width: '100px' })} variant="danger" disabled={true} label="test" />
+      </Box>
+      <Box title="badge">{<Badge count={222}>badge</Badge>}</Box>
+      <Box title="breadcrumb">
+        <BreadCrumb crumbs={['a', 'b', 'c']} onClick={(c) => console.log(c)} />
+      </Box>
+      <Box title="tooltip">
+        <Tooltip message="world">hello</Tooltip>
+      </Box>
+      <Box title="tag">
+        <Tag label="sampleTag" />
+        <Tag label="sampleTag" variant="rectangle" />
+        <Tag label="sampleTag" color="blue" />
+        <Tag label="sampleTag" variant="rectangle" color="blue" />
+        <Tag label="sampleTag" onClose={() => undefined} />
+        <Tag label="sampleTag" variant="rectangle" onClose={() => undefined} />
+        <Tag label="sampleTag" color="blue" onClose={() => undefined} />
+        <Tag label="sampleTag" variant="rectangle" color="blue" onClose={() => undefined} />
+      </Box>
     </div>
   )
 }
 
 export default App
+
+const Box = ({ children, title }: { children: ReactNode; title: string }) => {
+  const theme = useTheme()
+  return (
+    <div
+      className={css({
+        margin: 8,
+        border: '1px solid gray',
+        borderRadius: '6px',
+        padding: 16,
+      })}
+    >
+      <Typography
+        type="heading"
+        variant="h3"
+        className={css({ borderBottom: `1px solid ${theme.color.gray.light}` })}
+      >
+        {title}
+      </Typography>
+      <div
+        className={css({
+          marginTop: 16,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexWrap: 'wrap',
+          gap: '4px',
+        })}
+      >
+        {children}
+      </div>
+    </div>
+  )
+}
