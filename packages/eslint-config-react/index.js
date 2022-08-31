7dc1e03b2a6575cc0eecfc40c2e9fa9b89d44443
jklasd2001 <jklasd2001@naver.com>
2022-08-31 13:33:48 +0900

    feat: prettier/recommended 추가

--- packages/eslint-config-react/index.js
+++ packages/eslint-config-react/index.js
@@ -8,7 +8,7 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'plugin:import/recommended',
     'plugin:import/typescript',
-    'prettier',
+    "plugin:prettier/recommended",
   ],
   plugins: ['unused-imports'],
   settings: {
