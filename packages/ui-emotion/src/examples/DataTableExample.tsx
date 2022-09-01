96ae8e203e0f88113175cb61e81b7ea890503d78
Jinsoo Park <jinchuu1391@gmail.com>
2022-09-01 11:27:35 +0900

    chore: DataTable 더미 데이터 개수 늘림

--- packages/ui-emotion/src/examples/DataTableExample.tsx
+++ packages/ui-emotion/src/examples/DataTableExample.tsx
@@ -1,7 +1,7 @@
 import { useCallback } from 'react'
 import { DataTable, usePagination } from 'src/components'
 
-const originData = Array.from({ length: 30 }).map((_, index) => ({
+const originData = Array.from({ length: 200 }).map((_, index) => ({
   a: `a${index}`,
   b: `b${index}`,
   c: `c${index}`,
