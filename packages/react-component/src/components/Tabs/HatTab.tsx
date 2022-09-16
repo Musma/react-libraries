996589ca444c446ea56ad696727468677f6e3c32
Jinsoo Park <jinchuu1391@gmail.com>
2022-09-16 15:50:23 +0900

    style: HatTab 오른쪽 커브 아이콘 변경

--- packages/react-component/src/components/Tabs/HatTab.tsx
+++ packages/react-component/src/components/Tabs/HatTab.tsx
@@ -1,35 +1,48 @@
-import { css } from '@emotion/css'
-import { useTheme } from '@emotion/react'
 import { Fragment } from 'react'
 
+import { css } from '@emotion/css'
+import { useTheme } from '@emotion/react'
+
 import { Typography } from 'src/components'
-import { ReactComponent as TabLeftIcon } from './images/tab_left.svg'
+
+import { ReactComponent as CurveLeftIcon } from './images/curve_left.svg'
+import { ReactComponent as CurveRightIcon } from './images/curve_right.svg'
 import { TabProps } from './types'
-import { ReactComponent as TabRightIcon } from './images/tab_right.svg'
 
 export const HatTab = ({ title, selectedTab, setSelectedTab }: TabProps) => {
   const theme = useTheme()
   const inActive = (
-    <Typography type="subTitle" variant="subTitle2" className={css({padding: '10px 34px', color: theme.color.gray.darker})}>
+    <Typography
+      type="subTitle"
+      variant="subTitle2"
+      className={css({ padding: '10px 34px', color: theme.color.gray.darker })}
+    >
       {title}
     </Typography>
   )
   const active = (
     <Fragment>
-      <TabLeftIcon />
+      <CurveLeftIcon />
       <Typography
         type="subTitle"
         variant="subTitle2"
-        className={css({backgroundColor:theme.color.white.main, borderTop: `1px solid ${theme.color.gray.darker}`, padding: '10px 24px'})}
+        className={css({
+          backgroundColor: theme.color.white.main,
+          borderTop: `1px solid ${theme.color.gray.darker}`,
+          padding: '10px 24px',
+        })}
       >
         {title}
       </Typography>
-      <TabRightIcon />
+      <CurveRightIcon />
     </Fragment>
   )
 
   return (
-    <li className={css({display:'flex', height: '40px', cursor: 'pointer'})} onClick={() => setSelectedTab(title)}>
+    <li
+      className={css({ display: 'flex', height: '40px', cursor: 'pointer' })}
+      onClick={() => setSelectedTab(title)}
+    >
       {title === selectedTab ? active : inActive}
     </li>
   )
--- packages/react-component/src/components/Tabs/Tabs.tsx
+++ packages/react-component/src/components/Tabs/Tabs.tsx
@@ -1,20 +1,21 @@
-import { useTheme } from '@emotion/react'
-import { css } from '@emotion/css'
 import { useEffect, useState } from 'react'
+
+import { css } from '@emotion/css'
+import { useTheme } from '@emotion/react'
 
 import { HatTab } from './HatTab'
 import { RectTab } from './RectTab'
 
 interface TabsProps {
-    titles: string[]
-    handleSelectTab?: (title: string) => void
-    variant?: 'hat' | 'rect'
-  }
+  titles: string[]
+  handleSelectTab?: (title: string) => void
+  variant?: 'hat' | 'rect'
+}
 
 export const Tabs = ({ titles, handleSelectTab, variant = 'hat' }: TabsProps) => {
   const theme = useTheme()
   const [selectedTab, setSelectedTab] = useState(titles[0])
-  
+
   useEffect(() => {
     if (!handleSelectTab) return
     handleSelectTab(selectedTab)
@@ -22,7 +23,13 @@ export const Tabs = ({ titles, handleSelectTab, variant = 'hat' }: TabsProps) =>
 
   if (variant === 'hat') {
     return (
-      <ul className={css({display:'flex', alignItems:'center', borderBottom: `1px solid ${theme.color.gray.darker}`})}>
+      <ul
+        className={css({
+          display: 'flex',
+          alignItems: 'center',
+          borderBottom: `1px solid ${theme.color.gray.darker}`,
+        })}
+      >
         {titles.map((title) => (
           <HatTab
             key={title}
@@ -35,19 +42,26 @@ export const Tabs = ({ titles, handleSelectTab, variant = 'hat' }: TabsProps) =>
     )
   }
   return (
-    <ul className={css({display:'flex'})}>
+    <ul className={css({ display: 'flex' })}>
       {titles.map((title, index, array) => (
-        <div key={title} className={css({display:'flex', alignItems:'center'})}>
+        <div key={title} className={css({ display: 'flex', alignItems: 'center' })}>
           <RectTab
             key={title}
             title={title}
             selectedTab={selectedTab}
             setSelectedTab={() => setSelectedTab(title)}
           />
-          {index !== array.length - 1 && <div className={css({height:'16px', borderLeft: `1px solid ${theme.color.gray.darker}`})} />}
+          {index !== array.length - 1 && (
+            <div
+              className={css({
+                height: '16px',
+                borderLeft: `1px solid ${theme.color.gray.darker}`,
+              })}
+            />
+          )}
         </div>
       ))}
-      <div className={css({flex:1 , borderBottom: `1px solid ${theme.color.gray.darker}`})} />
+      <div className={css({ flex: 1, borderBottom: `1px solid ${theme.color.gray.darker}` })} />
     </ul>
   )
 }
