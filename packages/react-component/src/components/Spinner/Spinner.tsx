a8ef3639ad3ecb8d99e487624580a99b6a6f4e71
Jinsoo Park <jinchuu1391@gmail.com>
2022-09-29 13:07:09 +0900

    chore: 누락된 import 추가

--- packages/react-component/src/components/Spinner/Spinner.tsx
+++ packages/react-component/src/components/Spinner/Spinner.tsx
@@ -22,26 +22,20 @@ export const Spinner = ({ size = 'md' }: SpinnerProps) => {
       viewBox="0 0 16 16"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      className={css({
+      css={{
         '@keyframes spin': {
           to: {
             transform: 'rotate(360deg)',
           },
         },
         animation: 'spin 1s linear infinite',
-      })}
+      }}
       {...svgSize}
     >
-      <circle
-        cx={8}
-        cy={8}
-        r={7}
-        strokeWidth={2}
-        className={css({ stroke: theme.color.gray.main })}
-      />
+      <circle cx={8} cy={8} r={7} strokeWidth={2} css={{ stroke: theme.color.gray.main }} />
       <path
         d="M8 0a1 1 0 0 0 0 2 6 6 0 1 1-6 6 1 1 0 1 0-2 0 8 8 0 1 0 8-8Z"
-        className={css({ fill: theme.color.blue.light })}
+        css={{ fill: theme.color.blue.light }}
       />
     </svg>
   )
