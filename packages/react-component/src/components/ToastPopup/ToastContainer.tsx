5ffd81a1c0718d386fa923306644d894cb140afd
misun <mcoding777@naver.com>
2022-12-29 11:37:43 +0900

    refactor: react-toastify 마이그레이션 중

--- packages/react-component/src/components/ToastPopup/ToastContainer.tsx
+++ packages/react-component/src/components/ToastPopup/ToastContainer.tsx
@@ -1,22 +1,30 @@
-import { Fragment, useEffect, useState } from 'react'
+import { forwardRef, Fragment, useEffect } from 'react'
 
-import { IToastContainerProps, IToastPopupData } from '.'
+import { IToastPopupData, Toast, ToastContainerProps, useToastContainer } from '.'
 import { ToastPopup } from './ToastPopup'
-import { toastPopupManager } from './ToastPopupManager'
 
-export const ToastContainer = ({ height = '0px', newToastPopup }: IToastContainerProps) => {
-  const [list, setList] = useState<IToastPopupData[]>(toastPopupManager.list)
+export const ToastContainer = forwardRef<HTMLDivElement, ToastContainerProps>((props, ref) => {
+  // const [list, setList] = useState<IToastPopupData[]>(toastPopupManager.list)
 
-  const handleClose = (toastPopup: IToastPopupData) => {
-    setList(toastPopupManager.remove(toastPopup))
-  }
+  // const handleClose = (toastPopup: IToastPopupData) => {
+  //   setList(toastPopupManager.remove(toastPopup))
+  // }
 
-  // 들어오는 ToastPopup이 다를 때마다 ToastPopupManager에 추가하고 list 동기화
+  // // 들어오는 ToastPopup이 다를 때마다 ToastPopupManager에 추가하고 list 동기화
+  // useEffect(() => {
+  //   if (newToastPopup) {
+  //     setList(toastPopupManager.add(newToastPopup))
+  //   }
+  // }, [newToastPopup])
+
+  const { getToastToRender, containerRef, isToastActive } = useToastContainer(props)
+  const { height, newToastPopup, containerId } = props
+
   useEffect(() => {
-    if (newToastPopup) {
-      setList(toastPopupManager.add(newToastPopup))
+    if (ref) {
+      ;(ref as React.MutableRefObject<HTMLDivElement>).current = containerRef.current!
     }
-  }, [newToastPopup])
+  }, [])
 
   return newToastPopup ? (
     <div
@@ -26,23 +34,36 @@ export const ToastContainer = ({ height = '0px', newToastPopup }: IToastContaine
         right: 10,
         zIndex: 9999,
       }}
+      ref={containerRef}
     >
-      {list.map((item: IToastPopupData) => {
+      {getToastToRender((toastList: Toast[]) => {
         return (
-          <ToastPopup
-            key={item.id}
-            id={item.id}
-            title={item.title}
-            onCloseClick={() => handleClose(item)}
-            state={item.state}
-            mode={item.mode}
-            description={item?.description}
-            ref={item.ref}
-          />
+          <Fragment>
+            {toastList.map((item: Toast) => {
+              return (
+                <ToastPopup
+                  key={item.id}
+                  id={item.id}
+                  title={item.title}
+                  onCloseClick={() => handleClose(item)}
+                  state={item.state}
+                  mode={item.mode}
+                  description={item?.description}
+                  ref={item.ref}
+                />
+              )
+            })}
+          </Fragment>
         )
       })}
     </div>
   ) : (
     <Fragment />
   )
+})
+
+ToastContainer.displayName = 'ToastContainer'
+
+ToastContainer.defaultProps = {
+  height: '50px',
 }
