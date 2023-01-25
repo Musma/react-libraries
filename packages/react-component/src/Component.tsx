12aff125f55dad9ae8f86bba949b6bb3c1a604d1
misun <mcoding777@naver.com>
2023-01-25 16:38:11 +0900

    refactor: 예시 페이지에서 안쓰는 변수 삭제

--- packages/react-component/src/Component.tsx
+++ packages/react-component/src/Component.tsx
@@ -33,57 +33,7 @@ const options = [
   },
 ]
 
-const columns = [
-  {
-    columnName: 'id',
-    columnLabel: '테이블_게시글_순번',
-  },
-  {
-    columnName: 'deviceID',
-    columnLabel: '단말기_ID',
-  },
-  {
-    columnName: 'eol',
-    columnLabel: '단말기_EoL',
-  },
-  {
-    columnName: 'releaseDate',
-    columnLabel: '단말기_출고일자',
-  },
-  {
-    columnName: 'matchDate',
-    columnLabel: '단말기_매칭일자',
-  },
-  {
-    columnName: 'firmwareVersion',
-    columnLabel: '펌웨어 버전',
-  },
-  {
-    columnName: 'currentCommDate',
-    columnLabel: '단말기_통신일시',
-  },
-  {
-    columnName: 'deviceStatus',
-    columnLabel: '단말기_상태',
-  },
-]
-
-const tableData = Array.from({ length: 20 }).map((_, index) => ({
-  id: `${index + 1}`,
-  deviceID: '1a4e-6135-1abe-fc10-41c7',
-  eol: 'U22B25003089',
-  releaseDate: '2023-10-01',
-  matchDate: '2023-10-01',
-  firmwareVersion: <button>dkdkdkdk</button>,
-  currentCommDate: '2023-09-30 08:00',
-  deviceStatus: index < 10 ? '사용가능' : '사용불가',
-}))
-
 const Component = () => {
-  type DDD = {
-    date: string
-  }
-
   const navigate = useNavigate()
   const theme = useTheme()
   const [value, setValue] = useState('1')
@@ -105,6 +55,7 @@ const Component = () => {
     mode: 'dark',
   }
   const popupSample4: IToastPopupData = {
+    title: '위험위허멍위험위험위험위험',
     type: 'warning',
   }
 
