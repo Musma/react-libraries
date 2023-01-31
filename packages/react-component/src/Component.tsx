ce4240cdf4f4febe572f208ec8437ec00f791307
dave352 <dave@musma.net>
2023-01-31 16:13:44 +0900

    chore: test DatePicker Component

--- packages/react-component/src/Component.tsx
+++ packages/react-component/src/Component.tsx
@@ -1,62 +1,97 @@
 import { Fragment } from 'react'
 import { Controller } from 'react-hook-form'
 
+import { useTheme } from '@emotion/react'
 import { useFormSearch } from '@musma/react-utils'
 import { DateTime } from 'luxon'
 
-import { DatePicker, Grid } from './components'
+import { Card, DatePicker, Grid, SearchForm } from './components'
 
 const Component = () => {
-  const { control } = useFormSearch({
+  const theme = useTheme()
+  const { control, onSubmit, handleSubmit, getValues } = useFormSearch({
     useFormProps: {
       defaultValues: {
-        date: DateTime.now(),
-        range: [null, null],
+        curDate: DateTime.now().setZone('Asia/Seoul').toISO(),
+        defaultRangeDate: [
+          DateTime.now().setZone('Asia/Seoul').toISO(),
+          DateTime.now().setZone('Asia/Seoul').toISO(),
+        ],
+        nullRangeDate: [null, null],
       },
     },
 
     fetchAPI() {
-      //api
+      const { curDate, defaultRangeDate, nullRangeDate } = getValues()
+      if (curDate) {
+        console.log('date', curDate)
+      }
+      if (defaultRangeDate) {
+        console.log('defaultRangeDate', defaultRangeDate)
+      }
+      if (nullRangeDate.length > 0) {
+        console.log('nullRangeDate', nullRangeDate)
+      }
     },
   })
 
   return (
     <Fragment>
-      <Grid
-        itemWidth={200}
-        cols={2}
-        css={{
-          position: 'absolute',
-          left: '50%',
-          top: '50%',
-        }}
-      >
-        <Controller
-          name="date"
-          control={control}
-          render={({ field: { value, onChange, ...rest } }) => {
-            return <DatePicker label="일반" value={value} onChange={onChange} {...rest} />
-          }}
-        />
+      <Card css={{ padding: 120 }}>
+        <SearchForm onSubmit={handleSubmit(onSubmit)}>
+          <Grid
+            itemWidth={200}
+            cols={3}
+            css={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              columnGap: theme.spacing.sm,
+            }}
+          >
+            <Controller
+              name="curDate"
+              control={control}
+              render={({ field: { ...rest } }) => {
+                return <DatePicker label="기본 타입" {...rest} />
+              }}
+            />
+            <Controller
+              name="defaultRangeDate"
+              control={control}
+              render={({ field: { value, ...rest } }) => {
+                const [startDate, endDate] = value
+                return (
+                  <DatePicker
+                    label="레인지 타입(디폴트 o)"
+                    startDate={startDate}
+                    endDate={endDate}
+                    selectRange={true}
+                    {...rest}
+                  />
+                )
+              }}
+            />
+            <Controller
+              name="nullRangeDate"
+              control={control}
+              render={({ field: { value, ...rest } }) => {
+                const [startDate, endDate] = value ?? [null, null]
 
-        <Controller
-          name="range"
-          control={control}
-          render={({ field: { value, onChange, ...rest } }) => {
-            const [startDate, endDate] = value ?? [null, null]
-            return (
-              <DatePicker
-                label="레인지"
-                startDate={startDate}
-                endDate={endDate}
-                onChange={onChange}
-                selectRange={true}
-                {...rest}
-              />
-            )
-          }}
-        />
-      </Grid>
+                return (
+                  <DatePicker
+                    label="레인지 타입(디폴트 x)"
+                    startDate={startDate}
+                    endDate={endDate}
+                    selectRange={true}
+                    {...rest}
+                  />
+                )
+              }}
+            />
+          </Grid>
+        </SearchForm>
+      </Card>
     </Fragment>
   )
 }
