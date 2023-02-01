467226801ab7a95feafe870b8becff884633afca
dave352 <dave@musma.net>
2023-02-01 10:47:31 +0900

    chore: Component가 충돌나지 않도록 수정

--- packages/react-component/src/Component.tsx
+++ packages/react-component/src/Component.tsx
@@ -1,103 +1,132 @@
-import { Fragment } from 'react'
-import { Controller } from 'react-hook-form'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import { useTheme } from '@emotion/react'
-import { useFormSearch } from '@musma/react-utils'
-import { DateTime } from 'luxon'
 
-import { Card, DatePicker, Grid, SearchForm } from './components'
+import { Box } from 'src/elements'
+
+import { Button, Chip, IToastPopupData, Select, useToastContext } from './components'
+
+const options = [
+  {
+    label: '1114',
+    value: '1',
+  },
+  {
+    label: '2224',
+    value: '2',
+  },
+  {
+    label: '3335',
+    value: '3',
+  },
+  {
+    label: '4445',
+    value: '4',
+  },
+]
 
 const Component = () => {
+  const navigate = useNavigate()
   const theme = useTheme()
-  const { control, onSubmit, handleSubmit, getValues } = useFormSearch({
-    useFormProps: {
-      defaultValues: {
-        date: DateTime.now().setZone('Asia/Seoul').toISO(),
-        defaultRangeDate: [
-          DateTime.now().setZone('Asia/Seoul').toISO(),
-          DateTime.now().setZone('Asia/Seoul').toISO(),
-        ],
-        nullRangeDate: [null, null],
-      },
-    },
+  const [value, setValue] = useState('1')
 
-    fetchAPI() {
-      const { date, defaultRangeDate, nullRangeDate } = getValues()
-      if (date) {
-        console.log('date', date)
-      }
-      if (defaultRangeDate) {
-        console.log('defaultRangeDate', defaultRangeDate)
-      }
-      if (nullRangeDate.length > 0) {
-        console.log('nullRangeDate', nullRangeDate)
-      }
-    },
-  })
+  const { addToast, setLimit } = useToastContext()
+  const popupSample1: IToastPopupData = {
+    title: 'Error',
+    description: 'This is a warning notice about copywriting.',
+    mode: 'dark',
+    type: 'error',
+  }
+  const popupSample2: IToastPopupData = {
+    title: '잘했다임마',
+    description: '굿 잘 됨',
+    type: 'success',
+  }
+  const popupSample3: IToastPopupData = {
+    title: '정보 팝업인데 정보 음슴 ㅋ',
+    mode: 'dark',
+  }
+  const popupSample4: IToastPopupData = {
+    title: '위험위허멍위험위험위험위험',
+    type: 'warning',
+  }
 
   return (
-    <Fragment>
-      <Card css={{ padding: 120 }}>
-        <SearchForm onSubmit={handleSubmit(onSubmit)}>
-          <Grid
-            itemWidth={200}
-            cols={3}
-            css={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              columnGap: theme.spacing.sm,
-            }}
-          >
-            {/* DatePicker 기본 타입 */}
-            <Controller
-              name="date"
-              control={control}
-              render={({ field: { ...rest } }) => {
-                return <DatePicker label="기본 타입" {...rest} />
-              }}
-            />
+    <Box>
+      <Box
+        css={{
+          padding: theme.spacingUtil(100),
+          display: 'flex',
+          flexDirection: 'column',
+          gap: theme.spacing.md,
+        }}
+      >
+        <Select options={options} value={value} onChange={setValue} />
 
-            {/* DatePicker 레인지 타입 (defaultValue O) */}
-            <Controller
-              name="defaultRangeDate"
-              control={control}
-              render={({ field: { value, ...rest } }) => {
-                const [startDate, endDate] = value
-                return (
-                  <DatePicker
-                    label="레인지 타입(디폴트 o)"
-                    startDate={startDate}
-                    endDate={endDate}
-                    selectRange={true}
-                    {...rest}
-                  />
-                )
-              }}
-            />
+        <Select options={options} value={value} disabled={true} onChange={setValue} />
 
-            {/* DatePicker 레인지 타입 (defaultValue X) */}
-            <Controller
-              name="nullRangeDate"
-              control={control}
-              render={({ field: { value, ...rest } }) => {
-                const [startDate, endDate] = value ?? [null, null]
+        <Button
+          onClick={() => {
+            setValue('3')
+          }}
+        >
+          dkdkd
+        </Button>
+      </Box>
+      <Box css={{ display: 'flex', gap: '10px' }}>
+        <Chip
+          color={theme.colors.red.main}
+          shape="rounded"
+          onClick={() => {
+            addToast(popupSample1)
+          }}
+        >
+          토스트 팝업 1
+        </Chip>
+        <Chip
+          color={theme.colors.green.main}
+          shape="rounded"
+          onClick={() => {
+            addToast(popupSample2)
+          }}
+        >
+          토스트 팝업 2
+        </Chip>
+        <Chip
+          color={theme.colors.blue.main}
+          shape="rounded"
+          onClick={() => {
+            addToast(popupSample3)
+          }}
+        >
+          토스트 팝업 3
+        </Chip>
 
-                return (
-                  <DatePicker
-                    label="레인지 타입(디폴트 x)"
-                    startDate={startDate}
-                    endDate={endDate}
-                    selectRange={true}
-                    {...rest}
-                  />
-                )
-              }}
-            />
-          </Grid>
-        </SearchForm>
-      </Card>
-    </Fragment>
+        <Chip
+          color={theme.colors.orange.main}
+          shape="rounded"
+          onClick={() => {
+            addToast(popupSample4)
+          }}
+        >
+          토스트 팝업 4
+        </Chip>
+        <Chip
+          color={theme.colors.black.main}
+          onClick={() => {
+            const random = Math.floor(Math.random() * 10) + 1
+            console.log('random', random)
+            setLimit(random)
+          }}
+        >
+          토스트 팝업 limit 수를 랜덤으로 변경 (1~10 사이)
+        </Chip>
+        <Chip color={theme.colors.black.darker} onClick={() => navigate('toast')}>
+          토스트 팝업 페이지로 이동
+        </Chip>
+      </Box>
+    </Box>
   )
 }
 
