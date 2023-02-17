c22afbe5ab58d5a6f2b5d6f617cc770bf37cb7a6
dave352 <dave@musma.net>
2023-02-17 11:32:51 +0900

    chore: util RegExps 정규식 오류 수정

--- packages/react-utils/src/constants/RegExps.ts
+++ packages/react-utils/src/constants/RegExps.ts
@@ -2,10 +2,11 @@
  * 정규표현식을 모아놨습니다.
  */
 export const RegExps = {
-  ONLY_DIGIT: /^[0-9]*$/, // 숫자만
-  ONLY_ENGLISH: /^[a-zA-Z ]*$/, // 영문과 공백
-  ONLY_ENGLISH_AND_DIGIT: /^[a-zA-Z0-9 ]*$/, // 영문과 공백 + 숫자
-  ONLY_DIGIT_AND_DOT: /^[0-9|.]*$/, // 숫자와 Dot
-  EMAIL: /^[a-z0-9@|.]*$/, // 이메일
-  QRCODE: /^E[0-9][0-9][0-9][0-9][0-9]$/, // QR Code
+  ONLY_DIGIT: /^[0-9]*$/, //~> 숫자만
+  ONLY_DIGIT_AND_DASH: /^[0-9|-]*$/, //~> 숫자와 Dash
+  ONLY_DIGIT_AND_DOT: /^[0-9|.]*$/, //~> 숫자와 Dot
+  ONLY_EMAIL: /^[a-z0-9@|.]*$/, //~> 이메일
+  ONLY_ENGLISH: /^[a-zA-Z ]*$/, //~> 영문과 공백
+  ONLY_ENGLISH_AND_DIGIT: /^[a-zA-Z0-9 ]*$/, //~> 영문과 공백 + 숫자
+  QRCODE: /^E[0-9][0-9][0-9][0-9][0-9]$/, //~> QR Code
 } as const
