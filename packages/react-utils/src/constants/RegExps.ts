4d84a51cbcbb3d1f43bf630a0dc86dd525ff617f
dave352 <dave@musma.net>
2023-02-17 10:10:06 +0900

    feat: Email, DigitAndDot 정규표현식 추가

--- packages/react-utils/src/constants/RegExps.ts
+++ packages/react-utils/src/constants/RegExps.ts
@@ -5,5 +5,7 @@ export const RegExps = {
   ONLY_DIGIT: /^[0-9]*$/, // 숫자만
   ONLY_ENGLISH: /^[a-zA-Z ]*$/, // 영문과 공백
   ONLY_ENGLISH_AND_DIGIT: /^[a-zA-Z0-9 ]*$/, // 영문과 공백 + 숫자
+  ONLY_DIGIT_AND_DOT: /^[0-9|.]*$/, // 숫자와 Dot
+  EMAIL: /^[a-z0-9@|.]*$/, // 이메일
   QRCODE: /^E[0-9][0-9][0-9][0-9][0-9]$/, // QR Code
 } as const
