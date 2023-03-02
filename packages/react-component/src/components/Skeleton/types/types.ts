38e2d6de7993e78c92d932b22bc692aeff4f5041
Leona <leona@musma.net>
2023-03-02 13:51:19 +0900

    fix: Card Skeleton 컴포넌트 추가, 타입, 로직 등 기타 수정

--- packages/react-component/src/components/Skeleton/types/types.ts
+++ packages/react-component/src/components/Skeleton/types/types.ts
@@ -6,11 +6,8 @@ import { HTMLAttributes } from 'react'
  *
  * paragraph
  * @description variant가 table, list인 경우 출력 갯수(row). 기본 4줄
- *
- * backgroundColor
- * @description 스켈레톤 색상
  */
 export interface SkeletonProps extends HTMLAttributes<HTMLDivElement> {
-  variant?: 'circle' | 'rectangle' | 'image' | 'table' | 'list'
+  variant?: 'circle' | 'rectangle' | 'image' | 'table' | 'list' | 'card'
   paragraph?: number
 }
