03bfac9fab754aed9271dab90dac8c5962d2a740
jklasd2001 <jklasd2001@naver.com>
2023-03-10 09:19:42 +0900

    feat: [react-utils] DateUtil 함수 추가

---
+++ .changeset/big-apricots-behave.md
@@ -0,0 +1,5 @@
+---
+'@musma/react-utils': patch
+---
+
+feat: [react-utils] DateUtil 함수 추가
--- packages/react-utils/src/utils/DateUtil.ts
+++ packages/react-utils/src/utils/DateUtil.ts
@@ -1,12 +1,64 @@
 import { DateTime } from 'luxon'
 
-export function convertISOToDateTime(iso?: string | null, format = 'yyyy-MM-dd') {
-  if (iso) {
-    return DateTime.fromISO(iso).toFormat(format)
+/**
+ *
+ * @param iso
+ * @param format
+ * @returns
+ */
+
+const DEFAULT_FORMAT = 'yyyy-MM-dd'
+
+export const convertIsoToFormat = (isoDate?: string | null, format = DEFAULT_FORMAT) => {
+  if (isoDate) {
+    return DateTime.fromISO(isoDate).toFormat(format)
   }
   return undefined
 }
 
+/**
+ * @returns
+ */
+export const generateFromTo = (): [string, string] => {
+  const from = DateTime.now().startOf('month').startOf('day').toUTC().toISO()
+  const to = DateTime.now().endOf('day').toUTC().toISO()
+
+  return [from, to]
+}
+
+/**
+ * @param isoDate
+ * @returns ISO를 UTC로 변환하여 string으로 리턴합니다.
+ */
+export const convertIsoToUtc = (isoDate: string): string => {
+  return DateTime.fromISO(isoDate).toUTC().toISO()
+}
+
+/**
+ * @param isoDate ISO String
+ * @param format 변환할 날짜 포맷 ex)yyyy-MM-dd
+ * @returns ISO를 UTC로 변환한 후 포맷에 맞춰 string으로 리턴합니다.
+ */
+export const convertIsoToUtcToFormat = (isoDate: string, format = DEFAULT_FORMAT): string => {
+  return DateTime.fromISO(isoDate).toUTC().toFormat(format)
+}
+
+/**
+ * UTC to ISO 변환
+ * @param utcDate
+ * @param format default date(yyyy-MM-dd), dateTime(yyyy-MM-dd HH:mm:ss)
+ * @returns
+ */
+export const convertUtcToFormat = (utcDate?: string, format = 'yyyy-MM-dd') => {
+  return utcDate
+    ? DateTime.fromISO(new Date(utcDate).toISOString(), { zone: 'utc' }).toFormat(format)
+    : '-'
+}
+
+/**
+ *
+ * @returns
+ */
 export function getCurrentISOTime(): string {
   return DateTime.now().startOf('day').toISO()
 }
