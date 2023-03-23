9a46292bf6eb1915fa4b378018084b1603a06ccd
Leona <leona@musma.net>
2023-03-23 11:12:16 +0900

    feat: 정규식 추가

---
+++ .changeset/gorgeous-spiders-deliver.md
@@ -0,0 +1,6 @@
+---
+"@musma/react-component": patch
+"@musma/react-utils": patch
+---
+
+feat: 정규식 추가
--- packages/react-utils/src/constants/RegExps.ts
+++ packages/react-utils/src/constants/RegExps.ts
@@ -5,6 +5,7 @@ export const RegExps = {
   ONLY_DIGIT: /^[0-9]*$/, //~> 숫자만
   ONLY_DIGIT_AND_DASH: /^[0-9|-]*$/, //~> 숫자와 Dash
   ONLY_DIGIT_AND_DOT: /^[0-9|.]*$/, //~> 숫자와 Dot
+  ONLY_DIGIT_AND_DOT_DASH: /^[0-9|.|-]*$/, //~> 숫자와 Dot, Dash
   ONLY_EMAIL: /^[a-z0-9@|.]*$/, //~> 이메일
   ONLY_ENGLISH: /^[a-zA-Z ]*$/, //~> 영문과 공백
   ONLY_ENGLISH_AND_DIGIT: /^[a-zA-Z0-9 ]*$/, //~> 영문과 공백 + 숫자
