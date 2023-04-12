a7be5b469467e96c8313669234bbaf72f61e8bf8
dave352 <dave@musma.net>
2023-04-12 10:20:11 +0900

    feat: RegExp에 english와 dash, dot을 같이 사용할 수 있도록 추가

--- packages/react-utils/src/constants/RegExps.ts
+++ packages/react-utils/src/constants/RegExps.ts
@@ -6,8 +6,10 @@ export const RegExps = {
   ONLY_DIGIT_AND_DASH: /^[0-9|-]*$/, //~> 숫자와 Dash
   ONLY_DIGIT_AND_DOT: /^[0-9|.]*$/, //~> 숫자와 Dot
   ONLY_DIGIT_AND_DOT_DASH: /^[0-9|.|-]*$/, //~> 숫자와 Dot, Dash
-  ONLY_EMAIL: /^[a-z0-9@|.]*$/, //~> 이메일
   ONLY_ENGLISH: /^[a-zA-Z ]*$/, //~> 영문과 공백
+  ONLY_ENGLISH_AND_DASH: /^[a-zA-Z|-]*$/, //~> 영문과 Dash
+  ONLY_ENGLISH_AND_DOT: /^[a-zA-Z|.]*$/, //~> 영문과 Dot
+  ONLY_ENGLISH_AND_DOT_DASH: /^[a-zA-Z|.|-]*$/, //~> 영문과 Dot, Dash
   ONLY_ENGLISH_AND_DIGIT: /^[a-zA-Z0-9 ]*$/, //~> 영문과 공백 + 숫자
   QRCODE: /^E[0-9][0-9][0-9][0-9][0-9]$/, //~> QR Code
 } as const
