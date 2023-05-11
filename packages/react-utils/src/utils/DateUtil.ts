8c075290f5b7713b5c97f45387342b4944610620
dave352 <dave@musma.net>
2023-05-11 10:42:41 +0900

    chore: DateUtil 설명 주석 추가

--- packages/react-utils/src/utils/DateUtil.ts
+++ packages/react-utils/src/utils/DateUtil.ts
@@ -9,6 +9,12 @@ import { DateTime } from 'luxon'
 
 const DEFAULT_FORMAT = 'yyyy-MM-dd'
 
+/**
+ *
+ * @param isoDate (example) '2023-02-01T09:00:00.620+09:00'
+ * @param format (default) 'yyyy-MM-dd', (example) 'HH:mm:ss'
+ * @returns (return example) '2023-02-01'
+ */
 export const convertIsoToFormat = (isoDate?: string | null, format = DEFAULT_FORMAT) => {
   if (isoDate) {
     return DateTime.fromISO(isoDate).toFormat(format)
@@ -17,7 +23,8 @@ export const convertIsoToFormat = (isoDate?: string | null, format = DEFAULT_FOR
 }
 
 /**
- * @returns
+ * @description '월 초 ~ 금일' 까지 UTC로 변환한 시간을 반환합니다
+ * @returns (example) ['2023-01-30T15:00:00.000Z', '2023-02-01T14:59:59.999Z']
  */
 export const generateFromTo = (): [string, string] => {
   const from = DateTime.now().startOf('month').startOf('day').toUTC().toISO()
@@ -27,42 +34,46 @@ export const generateFromTo = (): [string, string] => {
 }
 
 /**
- * @param isoDate
- * @returns ISO를 UTC로 변환하여 string으로 리턴합니다.
+ * @description ISO를 UTC로 변환하여 string으로 리턴합니다.
+ * @param isoDate (example) '2023-02-01T09:00:00.620+09:00'
+ * @returns (return example) '2023-02-01T00:00:00.620Z'
  */
 export const convertIsoToUtc = (isoDate: string): string => {
   return DateTime.fromISO(isoDate).toUTC().toISO()
 }
 
 /**
- * @param isoDate ISO String
- * @param format 변환할 날짜 포맷 ex)yyyy-MM-dd
- * @returns ISO를 UTC로 변환한 후 포맷에 맞춰 string으로 리턴합니다.
+ * @description ISO를 UTC로 변환한 후 포맷에 맞춰 string으로 리턴합니다.
+ * @param isoDate (example) '2023-02-01T00:00:00.620+09:00'
+ * @param format (default) 'yyyy-MM-dd', (example) 'HH:mm:ss'
+ * @returns (return example) '2023-01-31'
  */
 export const convertIsoToUtcToFormat = (isoDate: string, format = DEFAULT_FORMAT): string => {
   return DateTime.fromISO(isoDate).toUTC().toFormat(format)
 }
 
 /**
- * UTC to ISO 변환
- * @param utcDate
- * @param format default date(yyyy-MM-dd), dateTime(yyyy-MM-dd HH:mm:ss)
- * @returns
+ * @description UTC를 ISO로 변환한 후 포맷에 맞춰 string으로 리턴합니다.
+ * @param utcDate (example) '2023-02-01T15:00:00.620Z'
+ * @param format (default) 'yyyy-MM-dd', (example) 'HH:mm:ss'
+ * @returns (return example) '2023-02-02'
  */
-export const convertUtcToFormat = (utcDate?: string, format = 'yyyy-MM-dd') => {
+export const convertUtcToFormat = (utcDate?: string, format = DEFAULT_FORMAT) => {
   return utcDate ? DateTime.fromISO(new Date(utcDate).toISOString()).toFormat(format) : '-'
 }
 
 /**
- *
+ * @description UTC를 ISO로 변환한 후 string으로 리턴합니다.
+ * @param utcDate (example) '2023-02-01T15:00:00.620Z'
+ * @returns (return example) '2023-02-02T00:00:00.620+09:00'
  */
 export const convertUtcToIso = (utcDate: string) => {
   return DateTime.fromISO(new Date(utcDate).toISOString()).toISO()
 }
 
 /**
- *
- * @returns
+ * @description 금일의 00시를 ISO로 변환하여 string으로 리턴합니다
+ * @returns (return example) '2023-02-02T00:00:00.620+09:00'
  */
 export function getCurrentISOTime(): string {
   return DateTime.now().startOf('day').toISO()
