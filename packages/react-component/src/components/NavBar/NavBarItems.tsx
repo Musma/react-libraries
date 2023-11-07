b29f30e949e5e1aa2a62843b0a547c15623e6f66
musma-sujan7 <mcoding777@musma.net>
2023-11-07 17:13:55 +0900

    refactor: NavBarItems props를 필수 값으로 변경 및 Fragment 추가

--- packages/react-component/src/components/NavBar/NavBarItems.tsx
+++ packages/react-component/src/components/NavBar/NavBarItems.tsx
@@ -6,9 +6,8 @@ import { NavBarItemsData, NavBarLink, NavBarList, useFoldingNavBarContext } from
 interface NavBarItemsProps {
   /**
    * 메뉴 스키마에 맞춰서 데이터를 전달하면 NavList와 NavLink를 자동으로 생성합니다. (폴딩 기능을 사용하면 필수 값)
-   * @optional
    */
-  items?: NavBarItemsData[]
+  items: NavBarItemsData[]
 }
 
 export const NavBarItems = ({ items }: NavBarItemsProps) => {
@@ -16,31 +15,37 @@ export const NavBarItems = ({ items }: NavBarItemsProps) => {
 
   const { isNavBarFolded, isMenuState, toggleMenuState } = useFoldingNavBarContext()
 
-  return items?.map((item) => {
-    if (item?.children) {
-      const isChildrenVisible = isMenuState[item.label] && !isNavBarFolded
-      const isChildrenActive = item.children.some((child) => {
-        if (!child.to) return false
-        return pathname.includes(child.to)
-      })
+  return (
+    <Fragment>
+      {items.map((item) => {
+        if (item?.children) {
+          const isChildrenVisible = isMenuState[item.label] && !isNavBarFolded
+          const isChildrenActive = item.children.some((child) => {
+            if (!child.to) return false
+            return pathname.includes(child.to)
+          })
 
-      return (
-        <Fragment key={item.label}>
-          <NavBarList
-            isChildrenActive={isChildrenActive}
-            icon={item.icon}
-            label={item.label}
-            active={isMenuState[item.label]}
-            onClick={() => toggleMenuState(item.label)}
-          />
-          {isChildrenVisible &&
-            item.children.map((child) => {
-              return <NavBarLink key={child.label} label={child.label} to={child.to ?? ''} />
-            })}
-        </Fragment>
-      )
-    }
+          return (
+            <Fragment key={item.label}>
+              <NavBarList
+                isChildrenActive={isChildrenActive}
+                icon={item.icon}
+                label={item.label}
+                active={isMenuState[item.label]}
+                onClick={() => toggleMenuState(item.label)}
+              />
+              {isChildrenVisible &&
+                item.children.map((child) => {
+                  return <NavBarLink key={child.label} label={child.label} to={child.to ?? ''} />
+                })}
+            </Fragment>
+          )
+        }
 
-    return <NavBarLink key={item.label} icon={item.icon} label={item.label} to={item.to ?? ''} />
-  })
+        return (
+          <NavBarLink key={item.label} icon={item.icon} label={item.label} to={item.to ?? ''} />
+        )
+      })}
+    </Fragment>
+  )
 }
